4319c849641a83afdb06ab7586dbba77e0935981
dev-bhaskar8 <bhaskar.syamal@gmail.com>
2022-03-01 01:34:13 +0530

    master

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+
+
+export default function Main(){
+
+    return(
+        <div >
+            <div className="hero">
+                <h1>Welcome to Vaastroopers</h1>
+                <br />
+                <h3>A community of memers, gamers and anime lovers who hope to form a billion dollars valuation DAO.</h3>
+                <br />
+                <div className="frame">
+                <input type="email" placeholder="Your ERC20 address."/>
+                <button>Future airdrops</button>
+                </div>
+            </div>
+            <div>
+                <img className="metaverse" src="./assets/metaverse.png" />
+            </div>
+
+        </div>
+    )
+
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+
+
+export default function Nav(){
+
+    return(
+        <nav>
+            <img src="./assets/trooperlogo.png"/>
+            <div>
+                <h2>Contact Us</h2>
+                <h2>About</h2>
+                <h2>Discord</h2>
+                <button>Connect your Metamask</button>
+            </div>       
+        </nav>
+
+
+    )
+
+
+}
