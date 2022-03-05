d7f8991b296147ad72cf230be3ee2adcad0a42f7
dev-bhaskar8 <bhaskar.syamal@gmail.com>
2022-03-05 06:45:49 +0530

    metamask

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+export default function About(){
+   return(
+    <div className="hero">
+    <h1>About</h1>
+    <br />
+    <br />
+    <h3>
+    Disclaimer: Please note that anything written in this document should not be taken as financial advice. Axie is a bleeding-edge game that's incorporating unfinished, risky, and highly experimental technology. Development priorities, roadmap, and features are subject to radical overhaul based on research, traction, feedback from the community, and a myriad of other factors.
+    </h3>
+    </div>
+     )
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,18 @@
 
 
-export default function Main(){
+export default function Main(props){
 
     return(
-        <div >
+        <div>
             <div className="hero">
                 <h1>Welcome to Vaastroopers</h1>
                 <br />
+                <br />
                 <h3>A community of memers, gamers and anime lovers who hope to form a billion dollars valuation DAO.</h3>
                 <br />
+                <br />
                 <div className="frame">
-                <input type="email" placeholder="Your ERC20 address."/>
+                <input type="email" placeholder="Your ERC20 address." value={props.address}></input>
                 <button>Future airdrops</button>
                 </div>
             </div>
